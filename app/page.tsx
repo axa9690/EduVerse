761542e0a21cd20227f7b54b1c53962c1e38b2a8
axa9690 <146138541+axa9690@users.noreply.github.com>
2024-10-02 02:10:38 -0500

    mn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import Login from './login/page';
-=======
-
->>>>>>> 1939daeebf9b4542dc156087ebf280ada3a84626
 
 const HomePage: React.FC = () => {
     return (
